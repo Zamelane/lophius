4aa174ce459cb5b3f9537b60fb605c3f8b6bbdb0
zamelane <zamelane@vk.com>
2025-02-19 02:32:03 +0700

    Добавил perfectionist

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,14 @@ import {db} from "@/db";
 import {usersTable} from "@/db/tables";
 
 export default async function Home() {
+  function b() {
+
+  }
+  function a() {
+
+  }
+  a();
+  b();
   const result = await db.select().from(usersTable)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from 'eslint-plugin-perfectionist';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
+
+const perfectionistConfig = {
+  plugins: { perfectionist, },
+  rules: {
+    'perfectionist/sort-imports': [
+      'error',
+      { type: 'line-length', order: 'asc', },
+    ],
+  },
+}
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  perfectionistConfig
 ];
 
 export default eslintConfig;
