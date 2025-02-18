9f762cc094356c18a3d33372d44fb5692f8cfaa2
zamelane <zamelane@vk.com>
2025-02-19 00:48:19 +0700

    Отказался от использования монорепы

--- apps/web/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-import path from "node:path";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@workspace/ui"],
-  outputFileTracingRoot: path.join(__dirname, '../../'),
-};
-
-export default nextConfig;
