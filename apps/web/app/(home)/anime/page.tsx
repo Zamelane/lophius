9eb8a287b32bdf05a8f6813eaf885c3bb7d5f6fd
Евгений Малинин <39529518+Zamelane@users.noreply.github.com>
2025-04-07 16:11:59 +0700

    FIX: Положение футера на страницах

--- apps/web/app/(home)/anime/page.tsx
+++ apps/web/app/(home)/anime/page.tsx
@@ -1,3 +1,9 @@
+import {ContentLayout} from "@/components/template-components/other/content-layout";
+
 export default function Page() {
-  return <p>Аниме</p>
+  return (
+    <ContentLayout className="px-4">
+      <p>Аниме</p>
+    </ContentLayout>
+  )
 }
--- apps/web/app/(home)/book/page.tsx
+++ apps/web/app/(home)/book/page.tsx
@@ -1,3 +1,9 @@
+import {ContentLayout} from "@/components/template-components/other/content-layout";
+
 export default function Page() {
-  return <p>Книги</p>
+  return (
+    <ContentLayout className="px-4">
+      <p>Книги</p>
+    </ContentLayout>
+  )
 }
