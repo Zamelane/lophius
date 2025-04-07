2fd01da57e02f65dcde58c83c6d3843f86cd146a
Евгений Малинин <39529518+Zamelane@users.noreply.github.com>
2025-04-07 16:11:59 +0700

    FIX: Положение футера на страницах

--- apps/web/app/(home)/comic/page.tsx
+++ apps/web/app/(home)/comic/page.tsx
@@ -1,3 +1,9 @@
+import {ContentLayout} from "@/components/template-components/other/content-layout";
+
 export default function Page() {
-  return <p>Комиксы</p>
+  return (
+    <ContentLayout className="px-4">
+      <p>Комиксы</p>
+    </ContentLayout>
+  )
 }
