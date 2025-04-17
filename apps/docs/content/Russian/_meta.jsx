41d6dedab39a856013b983a394d2764c306f8dc9
Евгений Малинин <39529518+Zamelane@users.noreply.github.com>
2025-04-18 03:45:36 +0700

    FIX

---
+++ apps/docs/content/Russian/_meta.jsx
@@ -0,0 +1,4 @@
+export default {
+  index: 'Вступление',
+  install: 'Установка'
+}
--- apps/web/i18n/request.ts
+++ apps/web/i18n/request.ts
@@ -1,3 +1,5 @@
+'use server'
+
 import { getRequestConfig } from "next-intl/server";
 import {getCurrentLocale} from "@/i18n/current-locale";
 
