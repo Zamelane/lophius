b3ead2393dee2ce1fec091f4d595e75ca758fbb2
Zamelane <zamelane@vk.com>
2025-05-02 08:47:56 +0700

    Начал настройку PWA

---
+++ apps/web/app/manifest.ts
@@ -0,0 +1,28 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Next.js PWA',
+    short_name: 'NextPWA',
+    description: 'A Progressive Web App built with Next.js',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    display_override: [
+      "window-controls-overlay"
+    ],
+    icons: [
+      {
+        src: '/manifest/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/manifest/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
