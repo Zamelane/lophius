d3adf846afbe82b3b4cf05edf668a89b292d8ba8
Zamelane <zamelane@vk.com>
2025-06-21 15:59:45 +0700

    Добавил варинты кнопок

--- packages/ui/src/web/button/button.stories.tsx
+++ packages/ui/src/web/button/button.stories.tsx
@@ -7,7 +7,29 @@ const meta = {
   argTypes: {
     variant: {
       control: 'select',
-      options: ['primary'],
+      options: [
+        'default',
+        'primary',
+        'filled'
+      ],
+    },
+    color: {
+      control: 'select',
+      options: [
+        'default',
+        'color-1',
+        'color-2',
+        'color-3',
+        'color-4',
+        'color-5'
+      ]
+    },
+    size: {
+      control: 'select',
+      options: [
+        'default',
+        'icon'
+      ]
     }
   },
   component: Button,
@@ -20,6 +42,47 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     variant: 'primary',
+    color: 'default',
     children: 'Click me'
+  }
+};
+
+export const Button1: Story = {
+  args: {
+    variant: "primary",
+    color: "color-1",
+    children: "Color 1"
+  }
+};
+
+export const Button2: Story = {
+  args: {
+    variant: "primary",
+    color: "color-2",
+    children: "Color 2"
+  }
+};
+
+export const Button3: Story = {
+  args: {
+    variant: "primary",
+    color: "color-3",
+    children: "Color 3"
+  }
+};
+
+export const Button4: Story = {
+  args: {
+    variant: "primary",
+    color: "color-4",
+    children: "Color 4"
+  }
+};
+
+export const Button5: Story = {
+  args: {
+    variant: "primary",
+    color: "color-5",
+    children: "Color 5"
   }
 };
--- packages/ui/src/web/button/button.tsx
+++ packages/ui/src/web/button/button.tsx
@@ -8,25 +8,36 @@ const buttonVariants = cva(
     "items-center justify-center gap-y-2",
     "px-3.5 py-0",
     "rounded-md border-none",
-    "bg-transparent",
-    "text-center leading-[30px] text-btn-default-text select-none",
+    "text-center leading-[30px] text-white select-none",
     "tap-transparent",
-    "active:scale-95 will-change-transform",
+    "active:scale-95 hover:opacity-90 active:opacity-80 will-change-transform",
     "transition-all duration-200 ease-[cubic-bezier(0.4,0,0.2,1)]"
   ),
   {
     variants: {
+      color: {
+        default: 'bg-btn-primary-default-filled',
+        'color-1': 'bg-btn-primary-filled-color-1',
+        'color-2': 'bg-btn-primary-filled-color-2',
+        'color-3': 'bg-btn-primary-filled-color-3',
+        'color-4': 'bg-btn-primary-filled-color-4',
+        'color-5': 'bg-btn-primary-filled-color-5'
+      },
       variant: {
-        primary: cn(
-          "bg-btn-primary-filled",
-          "text-white",
-          "hover:opacity-90 active:opacity-80"
-        )
+        default: 'bg-btn-default-filled text-btn-default-text',
+        primary: '',
+        filled: 'bg-btn-is-filled'
+      },
+      size: {
+        default: '',
+        icon: 'pl-0 pr-0 w-8'
       }
     },
     defaultVariants: {
-      variant: "primary"
-    }
+      variant: "primary",
+      color: 'default',
+      size: 'default'
+    },
   }
 )
 
@@ -36,11 +47,13 @@ type ButtonProps = ComponentProps<'button'> &
 export function Button({
   className,
   variant,
+  color,
+  size,
   ...props
 }: ButtonProps) {
   return (
     <button
-      className={cn(buttonVariants({ variant }), className)}
+      className={cn(buttonVariants({ variant, color, size }), className)}
       {...props}
     />
   )
