15f9cd5a395a96f8e01b72b5feb1e6abe7e0e2d5
Zamelane <zamelane@vk.com>
2026-08-10 18:38:23 +0700

    Update timestamps columns

--- apps/server/src/db/helpers/timestampsColumns.ts
+++ apps/server/src/db/helpers/timestampsColumns.ts
@@ -1,7 +1,11 @@
-import { text } from "drizzle-orm/sqlite-core/columns";
+import { text } from "drizzle-orm/sqlite-core";
 
 export const timestamps = {
-  updated_at: text(),
-  created_at: text().$defaultFn(() => new Date().toISOString()).notNull(),
+  created_at: text()
+    .$defaultFn(() => new Date().toISOString())
+    .notNull(),
+  updated_at: text()
+    .$defaultFn(() => new Date().toISOString())
+    .$onUpdateFn(() => new Date().toISOString()),
   deleted_at: text(),
-}
+};
